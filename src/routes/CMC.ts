e4ce9110982160f539c5c66d8b70ba435d187f24
Star <67430892+ayelai@users.noreply.github.com>
2021-01-05 17:32:24 -0600

    Add quotes endpoint for CMC API

--- src/routes/CMC.ts
+++ src/routes/CMC.ts
@@ -7,36 +7,9 @@ import fetch from "node-fetch";
 
 const router = Router();
 
-// function asyncWrapper(fn) {
-//     return async (req, res, next) => {
-//         try {
-//             const result = await Promise.resolve(fn(req));
-//             return res.send(result);
-//         } catch (err) {
-//             return console.log(err);
-//         }
-//     }
-// };
-
-// async function cmcFetch() {
-//     const requestOptions = {
-//         method: 'GET',
-//         headers: {
-//             'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY
-//         }
-//     };
-
-//     const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', requestOptions);
-//     const json = await response.json();
-//     return json;
-// }
-
-// Returns all latest listings from Coin Market Cap API
-// router.route('/').get(asyncWrapper(cmcFetch));
-
 
 /******************************************************************************
- *                      Get All Latest Listings - "GET /api/cmc/latest"
+ *                      Get Latest Listings - "GET /api/cmc/latest"
  ******************************************************************************/
 
 router.get('/latest', async (req: Request, res: Response) => {
@@ -48,7 +21,30 @@ router.get('/latest', async (req: Request, res: Response) => {
     };
     try {
         const response = await fetch(
-            'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+            'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap',
+            requestOptions
+        );
+        const json = await response.json();
+        return res.status(OK).json({ json });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        });
+    }
+});
+
+
+router.get('/quotes', async (req: Request, res: Response) => {
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY as string
+        }
+    };
+    const { queryParams } = req.body;
+    try {
+        const response = await fetch(
+            `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=${queryParams}`,
             requestOptions
         );
         const json = await response.json();
