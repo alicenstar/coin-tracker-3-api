8d9e470554bede446665c2dfbeb3b42992a05325
Star <67430892+alicenstar@users.noreply.github.com>
2021-03-03 15:27:47 -0600

    edit

--- src/Server.ts
+++ src/Server.ts
@@ -34,6 +34,12 @@ db.once('open', () => console.log('Connected to database'));
  *                              Set basic express settings
  ***********************************************************************************/
 
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
